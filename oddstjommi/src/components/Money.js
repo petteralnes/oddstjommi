33c2c49b562dce45a593bdf0547d5392b1b2d97c
Petter Alnes <petter.99@live.no>
2022-02-17 01:59:08 +0100

    Add Money component

---
+++ oddstjommi/src/components/Money.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from '../aids.jpg';
+
+function Money() {
+    return <>
+    <h1>Welcome to money</h1>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <img src={logo} className="App-logo" alt="logo" />
+    </>
+}
+
+export default Money 
